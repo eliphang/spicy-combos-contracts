201414b5a060b857af47ebcdf942f5f083434444
eliphang <eliphang@proton.me>
2023-09-06 11:31:15 +0000

    A start

--- Heap.sol
+++ Heap.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.7;
 
 // Adapted from https://github.com/zmitton/eth-heap/blob/916e884387a650ed83bce6b78f97c372b6dcc53f/contracts/Heap.sol
 
-struct Data {
-    Node[] nodes; // root is index 1; index 0 not used
+struct HeapData {
+    HeapNode[] nodes; // root is index 1; index 0 not used
     mapping(uint256 => uint256) heapIdToNodeIndex;
 }
 
-struct Node {
+struct HeapNode {
     uint256 heapId;
     uint256 priority;
 }
@@ -17,60 +17,60 @@ struct Node {
 library Heap {
     uint256 constant ROOT_INDEX = 1;
 
-    function insert(Data storage self, Node memory node) internal {
+    function insert(HeapData storage self, HeapNode memory node) internal {
         if (self.nodes.length == 0) {
             self.nodes.push(node);
         } else {
-            self.nodes.push(Node(0, 0)); // Create a new spot in the heap.
+            self.nodes.push(HeapNode(0, 0)); // Create a new spot in the heap.
             _siftUp(self, node, self.nodes.length - 1); // Sift up the new node, also filling in the new spot.
         }
     }
 
     /// Remove and return the node with the highest priority
-    function removeMax(Data storage self) internal returns (Node memory node) {
+    function removeMax(HeapData storage self) internal returns (HeapNode memory node) {
         node = self.nodes[ROOT_INDEX];
-        removeNode(self, node);
+        removeHeapNode(self, node);
     }
 
-    function removeNode(Data storage self, Node memory node) internal {
+    function removeHeapNode(HeapData storage self, HeapNode memory node) internal {
         uint256 nodeIndex = self.heapIdToNodeIndex[node.heapId];
         uint256 lastIndex = self.nodes.length - 1;
-        Node memory lastNode = self.nodes[lastIndex];
+        HeapNode memory lastHeapNode = self.nodes[lastIndex];
 
-        delete self.heapIdToNodeIndex[node.heapId]; // Delete the mapping from heapId to Node Index.
-        delete self.nodes[nodeIndex]; // Delete the Node struct for the removed node.
+        delete self.heapIdToNodeIndex[node.heapId]; // Delete the mapping from heapId to HeapNode Index.
+        delete self.nodes[nodeIndex]; // Delete the HeapNode struct for the removed node.
         self.nodes.pop(); // Reduce the heap size by one.
 
         if (nodeIndex != lastIndex) {
             // Put the last node in place of the removed node.
-            _siftUp(self, lastNode, nodeIndex); // A sift up might first be required.
+            _siftUp(self, lastHeapNode, nodeIndex); // A sift up might first be required.
             _siftDown(self, self.nodes[nodeIndex], nodeIndex); // Sift down the node that has taken the removed node's place.
         }
     }
 
-    function dump(Data storage self) internal view returns (Node[] storage) {
+    function dump(HeapData storage self) internal view returns (HeapNode[] storage) {
         return self.nodes;
     }
 
-    function getByHeapId(Data storage self, uint256 heapId)
+    function getByHeapId(HeapData storage self, uint256 heapId)
         internal
         view
-        returns (Node storage)
+        returns (HeapNode storage)
     {
         return self.nodes[self.heapIdToNodeIndex[heapId]];
     }
 
-    function getMax(Data storage self) internal view returns (Node storage) {
+    function getMax(HeapData storage self) internal view returns (HeapNode storage) {
         return self.nodes[ROOT_INDEX];
     }
 
-    function size(Data storage self) internal view returns (uint256) {
+    function size(HeapData storage self) internal view returns (uint256) {
         return self.nodes.length > 0 ? self.nodes.length - 1 : 0;
     }
 
     function _siftUp(
-        Data storage self,
-        Node memory node,
+        HeapData storage self,
+        HeapNode memory node,
         uint256 nodeIndex
     ) private {
         if (
@@ -85,8 +85,8 @@ library Heap {
     }
 
     function _siftDown(
-        Data storage self,
-        Node memory node,
+        HeapData storage self,
+        HeapNode memory node,
         uint256 nodeIndex
     ) private {
         uint256 length = self.nodes.length;
@@ -95,7 +95,7 @@ library Heap {
         if (length <= childIndex) {
             _insert(self, node, nodeIndex);
         } else {
-            Node memory largestChild = self.nodes[childIndex];
+            HeapNode memory largestChild = self.nodes[childIndex];
             if (
                 length > childIndex + 1 &&
                 self.nodes[childIndex + 1].priority > largestChild.priority
@@ -112,8 +112,8 @@ library Heap {
     }
 
     function _insert(
-        Data storage self,
-        Node memory node,
+        HeapData storage self,
+        HeapNode memory node,
         uint256 nodeIndex
     ) private {
         self.nodes[nodeIndex] = node;
--- SpicyCombos.sol
+++ SpicyCombos.sol
@@ -1,17 +1,144 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity >=0.8.7 <0.9.0;
+pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Heap.sol";
 
+/// @custom:repo https://github.com/eliphang/spicy-combos-contracts
 contract SpicyCombos is Ownable {
-    uint devFund = 0;
+    enum HelpingType {
+        DoubleHelping,
+        TimedHelping
+    }
+
+    struct Queue {
+        HeapData queue;
+        mapping(address => Helping) helpings;
+    }
+
+    struct Helping {
+        uint256 queueId;
+        HelpingType helpingType;
+        bool credit; // Is this a credit or a deposit?
+    }
+
+    struct Balance {
+        uint256 deposits;
+        uint256 credits;
+    }
+
+    uint256 devFund = 0;
+
+    uint256 immutable minValue;
+
+    mapping(uint256 => Queue) queues; // The keys are comboIds
+    mapping(address => Balance) balances;
+
+    error ValueOutOfRange(
+        string parameter,
+        uint256 allowedMinimum,
+        uint256 allowedMaximum
+    );
+
+    /// @param minValue_ the minimum value that can be deposited.
+    constructor(uint256 minValue_) {
+        minValue = minValue_;
+    }
 
     /// Withdraw all funds set aside for the dev fund.
     /// @dev The contract "owner" is considered the destination address of the dev fund.
     /// @dev The owner has no other privilege than to receive the amount set aside in the dev fund.
     function withdrawDevFund() external {
-        uint amount = devFund;
+        uint256 amount = devFund;
         devFund = 0; // don't allow the devFund to use reentrancy to withdraw more than its share
-        owner().call{value: amount}(""); // the devFund might be a smart contract, so forward all gas 
+        owner().call{value: amount}(""); // the devFund might be a smart contract, so forward all gas
+    }
+
+    /// Get a helping. The first six parameters uniquely define a spicy combo.
+    /// @param amountDigit1 first significant digit in the amount.
+    /// @param amountDigit2 second significant digit in the amount (or zero if there is only one significant digit).
+    /// @param amountZeros number of zeros to add to the minimum value for amount.
+    /// @param blocksDigit1 first significant digit in the number of blocks for timed helpings.
+    /// @param blocksDigit2 second significant digit in the number of blocks (or zero if there is only one significant digit).
+    /// @param blocksZeros number of zeros to add to the number of blocks.
+    /// @param doubleHelping Is this a double helping? If not, it's a timed helping.
+    /// @param premium the amount paid to advance in the queue. This can only come from deposits, not credits.
+    function addHelping(
+        uint256 amountDigit1,
+        uint256 amountDigit2,
+        uint256 amountZeros,
+        uint256 blocksDigit1,
+        uint256 blocksDigit2,
+        uint256 blocksZeros,
+        bool doubleHelping,
+        uint256 premium
+    ) external payable {
+        if (amountDigit1 == 0 || amountDigit1 > 9) {
+            revert ValueOutOfRange("amountDigit1", 1, 9);
+        }
+        if (amountDigit2 > 9) {
+            revert ValueOutOfRange("amountDigit2", 0, 9);
+        }
+        if (amountZeros > 9) {
+            revert ValueOutOfRange("amountZeros", 0, 9);
+        }
+        if (blocksDigit1 == 0 || blocksDigit1 > 9) {
+            revert ValueOutOfRange("blocksDigit1", 1, 9);
+        }
+        if (blocksDigit2 > 9) {
+            revert ValueOutOfRange("blocksDigit2", 0, 9);
+        }
+        if (blocksZeros > 6) {
+            revert ValueOutOfRange("blocksZeros", 0, 6);
+        }
+
+        balances[msg.sender].deposits += msg.value;
+
+        uint256 comboId = computeComboId(
+            amountDigit1,
+            amountDigit2,
+            amountZeros,
+            blocksDigit1,
+            blocksDigit2,
+            blocksZeros
+        );
+    }
+
+    function deposit() external payable {}
+
+    function withdraw() external {}
+
+    function computeComboId(
+        uint256 amountDigit1,
+        uint256 amountDigit2,
+        uint256 amountZeros,
+        uint256 blocksDigit1,
+        uint256 blocksDigit2,
+        uint256 blocksZeros
+    ) public pure returns (uint256) {
+        return
+            blocksZeros +
+            blocksDigit2 *
+            10 +
+            blocksDigit1 *
+            100 +
+            amountZeros *
+            1000 +
+            amountDigit2 *
+            10000 +
+            amountDigit1 *
+            100000;
+    }
+
+    function computeValue(
+        uint256 digit1,
+        uint256 digit2,
+        uint256 zeros
+    ) public pure returns (uint256) {
+        if (digit2 == 0) {
+            // only one significant digit
+            return digit1 * 10**zeros;
+        }
+        return (digit1 * 10 + digit2) * 10**zeros;
     }
 }
