491a92982989d008803d656260d52b634fcf0012
eliphang <eliphang@proton.me>
2023-09-12 03:20:53 +0000

    test withdraw

--- tests/SpicyCombosTest.js
+++ tests/SpicyCombosTest.js
@@ -1,115 +1,141 @@
-import {expect} from 'chai'
-import {ethers} from 'hardhat'
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
 
 const {
-  utils: {parseEther},
+    utils: { parseEther },
 } = ethers
 
 describe('deploy SpicyCombos contract', function () {
-  var sc, signers
-  const minValue = parseEther('.00001')
-
-  before(async function () {
-    signers = await ethers.getSigners()
-
-    const SpicyCombos = await ethers.getContractFactory('SpicyCombos')
-    sc = await SpicyCombos.deploy(minValue)
-    await sc.deployed()
-  })
-
-  it('minValue from getter should equal minValue supplied to constructor', async function () {
-    const minValueFromContract = await sc.minValue()
-    expect(minValueFromContract).to.equal(minValue)
-  })
-
-  describe('deposit()', function () {
-    var oldBalance, newBalance, gasCost
-    const depositAmount = parseEther('2')
+    var sc, signers
+    const minValue = parseEther('.00001')
 
     before(async function () {
-      const [owner] = signers
-      console.log('owner', owner)
-      oldBalance = await owner.getBalance()
-      const gasPrice = await owner.getGasPrice()
-      console.log(`old balance ${oldBalance}`)
-      const transaction = await sc.connect(owner).deposit({value: depositAmount})
-      const receipt = await transaction.wait()
-      gasCost = receipt.gasUsed.mul(gasPrice)
-      newBalance = await owner.getBalance()
-      console.log(`new balance ${newBalance}`)
-    })
-    it('available deposits should equal deposited amount', async function () {
-      const [owner] = signers
-      const addr = owner.address
-      const {0: availableDeposits} = await sc.balances(addr)
-      expect(availableDeposits).to.equal(depositAmount)
-    })
-    it('outside eth balance should decrease by deposited amount', function () {
-      expect(oldBalance.sub(depositAmount).sub(gasCost)).to.equal(newBalance)
-    })
-  })
+        signers = await ethers.getSigners()
 
-  describe('addHelping(), creating "Burning Chili Tacos with Spicy Cactus Nachos"', function () {
-    // .25 eth  "Burning Chili Tacos"
-    const amountDigit1 = 2
-    const amountDigit2 = 5
-    const amountZeros = 4
-
-    // 150 block time limit "Spicy Cactus Nachos"
-    const blocksDigit1 = 1
-    const blocksDigit2 = 5
-    const blocksZeros = 1
-
-    const premium1 = parseEther('.01')
-
-    before(async function () {
-      const [owner] = signers
-      const doubleHelping = true
-      const usingCredits = false
-      const creatorOnly = true
-      await sc
-        .connect(owner)
-        .addHelping(
-          amountDigit1,
-          amountDigit2,
-          amountZeros,
-          blocksDigit1,
-          blocksDigit2,
-          blocksZeros,
-          doubleHelping,
-          usingCredits,
-          creatorOnly,
-          premium1,
-          {value: parseEther('.26')}
-        )
+        const SpicyCombos = await ethers.getContractFactory('SpicyCombos')
+        sc = await SpicyCombos.deploy(minValue)
+        await sc.deployed()
     })
 
-    describe('withdrawDevFund()', function () {
-      var oldDevFundAmount, newDevFundAmount
-
-      before(async function () {
-        const [owner, addr1] = signers
-        oldDevFundAmount = await owner.getBalance()
-        await sc.connect(addr1).withdrawDevFund()
-        newDevFundAmount = await owner.getBalance()
-      })
-
-      it('the dev fund should increase by the premium deposited', function () {
-        expect(oldDevFundAmount.add(premium1)).to.equal(newDevFundAmount)
-      })
+    it('minValue from getter should equal minValue supplied to constructor', async function () {
+        const minValueFromContract = await sc.minValue()
+        expect(minValueFromContract).to.equal(minValue)
     })
-    
-    it('the active helping owner should be us', async function () {
-      const [owner] = signers
-      const {3: activeHelpingOwner} = await sc.comboInfo(
-        amountDigit1,
-        amountDigit2,
-        amountZeros,
-        blocksDigit1,
-        blocksDigit2,
-        blocksZeros
-      )
-      expect(activeHelpingOwner).to.equal(owner.address)
+
+    describe('deposit()', function () {
+        var oldBalance, newBalance, gasCost
+        const depositAmount = parseEther('2')
+
+        before(async function () {
+            const [owner] = signers
+            console.log('owner', owner)
+            oldBalance = await owner.getBalance()
+            const gasPrice = await owner.getGasPrice()
+            console.log(`old balance ${oldBalance}`)
+            const transaction = await sc.connect(owner).deposit({ value: depositAmount })
+            const receipt = await transaction.wait()
+            gasCost = receipt.gasUsed.mul(gasPrice)
+            newBalance = await owner.getBalance()
+            console.log(`new balance ${newBalance}`)
+        })
+        it('available deposits should equal deposited amount', async function () {
+            const [owner] = signers
+            const addr = owner.address
+            const { 0: availableDeposits } = await sc.balances(addr)
+            expect(availableDeposits).to.equal(depositAmount)
+        })
+        it('outside eth balance should decrease by deposited amount', function () {
+            expect(oldBalance.sub(depositAmount).sub(gasCost)).to.equal(newBalance)
+        })
+
+        describe('withdraw() what was deposited', function () {
+            var oldBalance, newBalance, gasCost
+
+            before(async function () {
+                const [owner] = signers
+                console.log('owner', owner)
+                oldBalance = await owner.getBalance()
+                const gasPrice = await owner.getGasPrice()
+                console.log(`old balance ${oldBalance}`)
+                const transaction = await sc.connect(owner).withdraw(depositAmount)
+                const receipt = await transaction.wait()
+                gasCost = receipt.gasUsed.mul(gasPrice)
+                newBalance = await owner.getBalance()
+                console.log(`new balance ${newBalance}`)
+            })
+            it('available deposits should equal zero', async function () {
+                const [owner] = signers
+                const addr = owner.address
+                const { 0: availableDeposits } = await sc.balances(addr)
+                expect(availableDeposits).to.equal(0)
+            })
+            it('outside eth balance should increase by withdrawn amount', function () {
+                expect(oldBalance.add(depositAmount).sub(gasCost)).to.equal(newBalance)
+            })
+        })
     })
-  })
+
+    describe('addHelping(), creating "Burning Chili Tacos with Spicy Cactus Nachos"', function () {
+        // .25 eth  "Burning Chili Tacos"
+        const amountDigit1 = 2
+        const amountDigit2 = 5
+        const amountZeros = 4
+
+        // 150 block time limit "Spicy Cactus Nachos"
+        const blocksDigit1 = 1
+        const blocksDigit2 = 5
+        const blocksZeros = 1
+
+        const premium1 = parseEther('.01')
+
+        before(async function () {
+            const [owner] = signers
+            const doubleHelping = true
+            const usingCredits = false
+            const creatorOnly = true
+            await sc
+                .connect(owner)
+                .addHelping(
+                    amountDigit1,
+                    amountDigit2,
+                    amountZeros,
+                    blocksDigit1,
+                    blocksDigit2,
+                    blocksZeros,
+                    doubleHelping,
+                    usingCredits,
+                    creatorOnly,
+                    premium1,
+                    { value: parseEther('.26') }
+                )
+        })
+
+        describe('withdrawDevFund()', function () {
+            var oldDevFundAmount, newDevFundAmount
+
+            before(async function () {
+                const [owner, addr1] = signers
+                oldDevFundAmount = await owner.getBalance()
+                await sc.connect(addr1).withdrawDevFund()
+                newDevFundAmount = await owner.getBalance()
+            })
+
+            it('the dev fund should increase by the premium deposited', function () {
+                expect(oldDevFundAmount.add(premium1)).to.equal(newDevFundAmount)
+            })
+        })
+
+        it('the active helping owner should be us', async function () {
+            const [owner] = signers
+            const { 3: activeHelpingOwner } = await sc.comboInfo(
+                amountDigit1,
+                amountDigit2,
+                amountZeros,
+                blocksDigit1,
+                blocksDigit2,
+                blocksZeros
+            )
+            expect(activeHelpingOwner).to.equal(owner.address)
+        })
+    })
 })
